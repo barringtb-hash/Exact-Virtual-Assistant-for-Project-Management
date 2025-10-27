8e91a9d60c05ad95034b5c9fdee820fb1436473e
barringtb-hash <barringtb@gmail.com>
2025-10-27 14:02:03 -0700

    Normalize charter payload on client & server; reduce validation noise

--- src/utils/getBlankCharter.js
+++ src/utils/getBlankCharter.js
@@ -1,19 +1,5 @@
-const getBlankCharter = () => ({
-  project_name: "",
-  sponsor: "",
-  project_lead: "",
-  start_date: "",
-  end_date: "",
-  vision: "",
-  problem: "",
-  description: "",
-  scope_in: [],
-  scope_out: [],
-  risks: [],
-  assumptions: [],
-  milestones: [],
-  success_metrics: [],
-  core_team: [],
-});
+import { createBlankCharter } from "../../lib/charter/normalize.js";
+
+const getBlankCharter = () => createBlankCharter();
 
 export default getBlankCharter;

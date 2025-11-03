29fc8f488658372c5b8cc27f66e8183ebf7310b7
barringtb-hash <barringtb@gmail.com>
2025-11-03 10:02:05 -0800

    Restore meter class alias for tests

--- src/components/MicButton.tsx
+++ src/components/MicButton.tsx
@@ -224,7 +224,11 @@ export default function MicButton({
           <path d="M12 14a3 3 0 0 0 3-3V6a3 3 0 1 0-6 0v5a3 3 0 0 0 3 3zm5-3a5 5 0 0 1-10 0H5a7 7 0 0 0 14 0h-2zM11 19h2v3h-2z" />
         </svg>
       </span>
-      <div className="mic-button__meter-rail" data-test="voice-meter" aria-hidden>
+      <div
+        className="mic-button__meter-rail mic-button__meter"
+        data-test="voice-meter"
+        aria-hidden
+      >
         <div ref={meterRef} className="mic-button__meter-fill" aria-hidden />
       </div>
     </button>

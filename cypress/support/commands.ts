83b61b9da751ec757ce5d8fcf432e84591018b22
barringtb-hash <barringtb@gmail.com>
2025-11-06 13:13:21 -0800

    Refine Cypress media device stubbing

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -8,6 +8,11 @@ declare global {
       getComposerInput(): Chainable<
         JQuery<HTMLInputElement | HTMLTextAreaElement>
       >;
+      restoreGUM(): Chainable<void>;
+      stubGUMSuccess(stream?: MediaStream): Chainable<sinon.SinonStub>;
+      stubGUMReject(
+        errorName: "NotAllowedError" | "NotFoundError"
+      ): Chainable<sinon.SinonStub>;
     }
   }
 }
@@ -41,6 +46,84 @@ Cypress.Commands.add("getComposerInput", () => {
       );
     });
 });
+
+Cypress.Commands.add("restoreGUM", () => {
+  cy.window({ log: false }).then((win) => {
+    const gum = win.navigator?.mediaDevices?.getUserMedia as
+      | undefined
+      | (((...args: unknown[]) => Promise<MediaStream>) & {
+          restore?: () => void;
+        });
+
+    if (gum && typeof gum.restore === "function") {
+      gum.restore();
+    }
+  });
+});
+
+const ensureMediaDevices = (win: Window) => {
+  const navigatorWithDevices = win.navigator as Navigator & {
+    mediaDevices?: MediaDevices;
+  };
+
+  if (!navigatorWithDevices.mediaDevices) {
+    navigatorWithDevices.mediaDevices = {} as MediaDevices;
+  }
+
+  return navigatorWithDevices.mediaDevices;
+};
+
+Cypress.Commands.add("stubGUMSuccess", (stream?: MediaStream) => {
+  return cy.window({ log: false }).then((win) => {
+    const mediaDevices = ensureMediaDevices(win);
+    const existing = mediaDevices.getUserMedia as unknown as {
+      restore?: () => void;
+    } | undefined;
+
+    if (existing?.restore) {
+      existing.restore();
+    }
+
+    const resolvedStream = stream
+      ? stream
+      : typeof win.MediaStream === "function"
+      ? new win.MediaStream()
+      : ({} as MediaStream);
+
+    const stub = Cypress.sinon
+      .stub(mediaDevices, "getUserMedia")
+      .callsFake(() => Promise.resolve(resolvedStream));
+
+    return cy.wrap(stub, { log: false });
+  });
+});
+
+Cypress.Commands.add(
+  "stubGUMReject",
+  (errorName: "NotAllowedError" | "NotFoundError") => {
+    return cy.window({ log: false }).then((win) => {
+      const mediaDevices = ensureMediaDevices(win);
+      const existing = mediaDevices.getUserMedia as unknown as {
+        restore?: () => void;
+      } | undefined;
+
+      if (existing?.restore) {
+        existing.restore();
+      }
+
+      const rejection =
+        typeof win.DOMException === "function"
+          ? new win.DOMException(errorName, errorName)
+          : Object.assign(new Error(errorName), { name: errorName });
+
+      const stub = Cypress.sinon
+        .stub(mediaDevices, "getUserMedia")
+        .callsFake(() => Promise.reject(rejection));
+
+      return cy.wrap(stub, { log: false });
+    });
+  }
+);
 
 Cypress.Commands.add("waitForAppReady", () => {
   cy.visit("/");

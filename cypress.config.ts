2d8a6dc1a7c386dd001b47e83942930fc5938e71
barringtb-hash <barringtb@gmail.com>
2025-11-06 16:48:48 -0800

    Fix Cypress baseUrl fallback to preview port

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "cypress";
 
-const baseUrl = process.env.CYPRESS_BASE_URL ?? "http://localhost:3000";
+const baseUrl = process.env.CYPRESS_BASE_URL ?? "http://localhost:5173";
 
 export default defineConfig({
   e2e: {
